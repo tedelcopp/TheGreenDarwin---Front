80950770a74d7cf2c5b7d798be3b7a11d7a09d67
tedelcopp <tomas.edelcopp@gmail.com>
2023-05-19 20:12:36 -0300

    wip

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-
+import Contact from "../ContactForm/ContactForm";
 import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "../Login/Login";
 import LogoutButton from "../Logout/Logout";
@@ -16,17 +16,25 @@ export default function NavBar() {
   const { isAuthenticated } = useAuth0();
   const { user } = useAuth0();
   //usar la logica de abajo cada vez que se quiera chequear si el user es admin, retorna un booleano
-  const isAdmin = user && user["https://pf-henry-front-one.vercel.app/roles"].includes("Admin");
+  const isAdmin =
+    user &&
+    user["https://pf-henry-front-one.vercel.app/roles"].includes("Admin");
   // user && console.log(user)
 
   let navigate = useNavigate();
 
   const handleClickMarket = () => {
-    window.location.reload()
+    window.location.reload();
   };
 
   const handleClickHome = (e) => {
-    navigate("/");
+    if (window.location.pathname === "/") {
+      e.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
   };
 
   return (
@@ -49,25 +57,38 @@ export default function NavBar() {
               About Us
             </Link>
             <Link to="/blogs" className={style.btn_left}>
-              Blog
+              Blogs
+            </Link>
+            <Link to="/" className={style.btn_left}>
+              Contact Us
             </Link>
           </div>
 
           <div className={style.right}>
-            {isAuthenticated ? <>
-              <div className={style.userName}> Hello, {user.nickname}!
-                <BurgerMenu /></div>
-            </>
-              : <LoginButton className={style.btn_right} />}
-            {isAdmin && 
-              <Link to='/admin' className={style.btn_right}>Admin dashboard</Link>
-            }
-            <Link to="/cart" className={style.btn_right} onClick={window.location.reload}>
-
+            {isAuthenticated ? (
+              <>
+                <div className={style.userName}>
+                  {" "}
+                  Hello, {user.nickname}!
+                  <BurgerMenu />
+                </div>
+              </>
+            ) : (
+              <LoginButton className={style.btn_right} />
+            )}
+            {isAdmin && (
+              <Link to="/admin" className={style.btn_right}>
+                Admin dashboard
+              </Link>
+            )}
+            <Link
+              to="/cart"
+              className={style.btn_right}
+              onClick={window.location.reload}
+            >
               <FontAwesomeIcon
                 icon={faCartShopping}
                 className={style.icon}
-                
                 alt="Shopping cart icon"
               />
             </Link>
